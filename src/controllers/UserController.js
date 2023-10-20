0f609c3d5a534f741eb93ab7d44b649b698e8e03
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2023-10-20 10:12:34 -0300

    adicionado controle middleware token

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,10 @@ class HomeController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      return res.json({
-        novoUser,
-      });
+      const { id, nome, email } = novoUser;
+      return res.json(
+        { id, nome, email },
+      );
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -16,7 +17,7 @@ class HomeController {
 
   async index(req, res) {
     try {
-      const users = await User.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -25,9 +26,9 @@ class HomeController {
 
   async show(req, res) {
     try {
-      const { id } = req.params;
-      const user = await User.findByPk(id);
-      return res.json(user);
+      const user = await User.findByPk(req.params.id);
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
@@ -35,14 +36,7 @@ class HomeController {
 
   async update(req, res) {
     try {
-      const { id } = req.params;
-      if (!id) {
-        return res.status(400).json({
-          errors: ['ID não informado.'],
-        });
-      }
-
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
@@ -51,7 +45,8 @@ class HomeController {
       }
 
       const novosDados = await user.update(req.body);
-      return res.json(novosDados);
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -61,14 +56,7 @@ class HomeController {
 
   async delete(req, res) {
     try {
-      const { id } = req.params;
-      if (!id) {
-        return res.status(400).json({
-          errors: ['ID não informado.'],
-        });
-      }
-
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 import userController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
-router.post('/', userController.store);
-router.get('/', userController.index);
+// apenas para estudo
 router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.get('/', loginRequired, userController.index);
+
+router.post('/', userController.store);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
 
 export default router;
